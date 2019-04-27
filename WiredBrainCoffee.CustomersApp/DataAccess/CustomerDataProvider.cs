355307142939e714dd8154f1a15aef2a6433eb5b
Radek Kurek <r.kurek@sputniksoftware.com>
2019-04-27 10:38:25 +0200

    layout, observable

---
+++ WiredBrainCoffee.CustomersApp/DataAccess/CustomerDataProvider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Newtonsoft.Json;
+using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp.DataAccess
+{
+    public class CustomerDataProvider
+    {
+        private static readonly string _customerFileName = "customers.json";
+        private static readonly StorageFolder _localFolder = ApplicationData.Current.LocalFolder;
+
+        public async Task<IEnumerable<Customer>> LoadCustomersAsync()
+        {
+            var storageFile = await _localFolder.TryGetItemAsync(_customerFileName) as StorageFile;
+            List<Customer> customerList = null;
+
+            if (storageFile == null) // jeśli brak pliku to utwórz nową listę
+            {
+                customerList = new List<Customer>
+                {
+                    new Customer{FirstName = "Jan", LastName = "Nowak", IsDeveloper = true},
+                    new Customer{FirstName = "Michał", LastName = "Kwiatek", IsDeveloper = true},
+                    new Customer{FirstName = "Alina", LastName = "Halny", IsDeveloper = true},
+                    new Customer{FirstName = "Wojtek", LastName = "Gawron", IsDeveloper = false},
+                    new Customer{FirstName = "Anna", LastName = "Wróbel", IsDeveloper = true},
+                    new Customer{FirstName = "Mirosław", LastName = "Lis", IsDeveloper = false}
+                };
+            }
+            else //inaczej czytaj z dżejsona
+            {
+                using (var stream = await storageFile.OpenAsync(FileAccessMode.Read)) //stream
+                {
+                    using (var dataReader = new DataReader(stream))
+                    {
+                        await dataReader.LoadAsync((uint) stream.Size); //czytaj dane ze stream
+                        var json = dataReader.ReadString((uint) stream.Size); //czytaj string
+                        customerList = JsonConvert.DeserializeObject<List<Customer>>(json); //deserializuj string jsona
+                    }
+                }
+            }
+
+            return customerList;
+        }
+
+        public async Task SaveCustomersAsync(IEnumerable<Customer> customers)
+        {
+            var storageFile =
+                await _localFolder.CreateFileAsync(_customerFileName, CreationCollisionOption.ReplaceExisting);
+
+            using (var stream = await storageFile.OpenAsync(FileAccessMode.ReadWrite)) //otwórz stream
+            {
+                using (var dataWriter = new DataWriter(stream))
+                {
+                    var json = JsonConvert.SerializeObject(customers, Formatting.Indented);
+                    dataWriter.WriteString(json);
+                    await dataWriter.StoreAsync();
+                }
+            }
+        }
+    }
+}
--- WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
+++ WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -1,30 +1,101 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.ApplicationModel;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using WiredBrainCoffee.CustomersApp.DataAccess;
+using WiredBrainCoffee.CustomersApp.Model;
 
 namespace WiredBrainCoffee.CustomersApp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        private CustomerDataProvider _customerDataProvider;
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += OnLoadedAsync;
+            App.Current.Suspending += CurrentOnSuspending;
+            _customerDataProvider = new CustomerDataProvider();
+        }
+
+        private async void CurrentOnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral(); //opóźniamy zamknięcie aplikacji
+
+            await _customerDataProvider.SaveCustomersAsync(customerListView.Items.OfType<Customer>());
+
+            deferral.Complete(); //poinformuj, że można już zamknąć
+        }
+
+        private async void OnLoadedAsync(object sender, RoutedEventArgs e)
+        {
+            customerListView?.Items?.Clear();
+
+            var customers = await _customerDataProvider.LoadCustomersAsync();
+            foreach (var customer in customers)
+            {
+                customerListView.Items.Add(customer);
+            }
+        }
+
+        private async void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var messageDialog = new MessageDialog("Customer Added");
+            await messageDialog.ShowAsync();
+        }
+
+        private async void ButtonRemoveCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var messageDialog = new MessageDialog("Customer Removed");
+            await messageDialog.ShowAsync();
+        }
+
+        private void ButtonMoveCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            int column = Grid.GetColumn(customerListGrid); //pobierz kolumnę do której należy Grid
+            int newColumn = column == 0 ? 2 : 0;
+
+            Grid.SetColumn(customerListGrid, newColumn); // ustaw kolumnę dla grida
+
+            SymbolIcon.Symbol = newColumn == 0 ? Symbol.Forward : Symbol.Back;
+        }
+
+        private void CustomerListView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var customer = customerListView.SelectedItem as Customer;
+
+            txtFirstName.Text = customer?.FirstName ?? "";
+            txtLastName.Text = customer?.LastName ?? "";
+            isDeveloperBox.IsChecked = customer?.IsDeveloper ?? false;
+        }
+
+        private void TxtFirstName_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void TxtLastName_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+           UpdateCustomer();
+        }
+
+        private void IsDeveloperBox_OnCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void UpdateCustomer()
+        {
+            var selectedCustomer = customerListView.SelectedItem as Customer;
+            if (selectedCustomer != null)
+            {
+                selectedCustomer.FirstName = txtFirstName.Text;
+                selectedCustomer.LastName = txtLastName.Text;
+                selectedCustomer.IsDeveloper = isDeveloperBox.IsChecked.GetValueOrDefault();
+            }
         }
     }
 }
